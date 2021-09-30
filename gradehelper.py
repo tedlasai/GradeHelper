0234efc884a5481e8d747c43dfdb1d8fa3cf45ae
Justin So <41768142+JustinSo1@users.noreply.github.com>
2021-09-30 00:25:14 -0400

    Loading student information into corresponding form boxes and replace WD with student files (#4)
    * Able to load students in sequential order
    
    * Prepopulate window with student information and load files into working directory
    
    * Extract loading files into working directory functionality into own method
    
    * Create message box when failing to delete files in WD
    
    * Remove pycache and idea files and ignore them in future commits
    
    * Add error checking for loading student information
    
    * Write all grades to single CSV file. (#5)
    
    Add functionality for method 'compileReport' to write out grades and student numbers to a single csv
    
    * Create message boxes when student ID does not exist and when all students are graded (#8)
    
    * Create message boxes when student ID does not exist and when all students are graded
    
    * Refactor to use one grade box instead of multiple for grading (#10)
    
    * Clear grades when loading next student
    
    * Add feedback input field (#11)
    
    * Add feedback input field
    
    * Use feedback in final grades csv
    
    * Remove printing of all directories
    
    Co-authored-by: Ian Mac <13894371+itsianmac@users.noreply.github.com>

--- gradehelper.py
+++ gradehelper.py
@@ -1,36 +1,67 @@
+import csv
 import os
-import constants
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QLabel, QLineEdit, QHBoxLayout, QWidget, QVBoxLayout, QPushButton
+import shutil
+import tokenize
+from bisect import bisect_left
+
 import pandas as pd
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QLabel, QLineEdit, QHBoxLayout, QWidget, QVBoxLayout, QPushButton, QMessageBox
 
-for file in sorted(os.listdir(constants.LAB_DIRECTORY)):
-    print(file)
+import constants
 
-#create working folder to copy files that I'm viewing to
+# create working folder to copy files that I'm viewing to
 if not os.path.exists(constants.WORKING_DIRECTORY):
     os.makedirs(constants.WORKING_DIRECTORY, exist_ok=True)
 
-#pyqt window
+
+# https://stackoverflow.com/questions/185936/how-to-delete-the-contents-of-a-folder
+def deleteDirectoryContent(folder):
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            createMessagePopUpBox(f"Failed to delete {file_path}. Reason:{e}. Check student directory")
+
+
+def findStudentID(text):
+    head = text.rstrip('0123456789')
+    tail = text[len(head):]
+    return tail
+
+
+def createMessagePopUpBox(text):
+    msg = QMessageBox()
+    msg.setWindowTitle("Error")
+    msg.setText(text)
+    msg.setIcon(QMessageBox.Critical)
+    msg.exec_()
+
+
+# pyqt window
 class Window(QtWidgets.QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.studentDirectories = os.listdir(constants.LAB_DIRECTORY)
-        self.gradedStudentDirectories = [False] * len(self.studentDirectories) #create a list of equal length as student directories that tracks if we have viewed it
+        self.studentDirectories = sorted(os.listdir(constants.LAB_DIRECTORY))
+        # create a list of equal length as student directories that tracks if we have viewed it
+        self.gradedStudentDirectories = [False] * len(self.studentDirectories)
         self.currentStudentGradedIndex = 0
         self.currentStudentDirectory = ""
         self.currentStudentGradesSubmitted = False
-        self.programStarted = True #just a flag when we start program
+        self.programStarted = True  # just a flag when we start program
 
         self.usernameLayoutText = QLabel("Student Username: ", self)
         self.usernameLayoutTextSetBox = QLineEdit(self)
-        self.usernameLayoutTextSetBox.setText("0")
+        self.usernameLayoutTextSetBox.setText("")
 
         self.usernameLayout = QHBoxLayout(self)
         self.usernameLayout.addWidget(self.usernameLayoutText)
         self.usernameLayout.addWidget(self.usernameLayoutTextSetBox)
-
 
         self.idLayoutText = QLabel("Student ID: ", self)
         self.idLayoutTextSetBox = QLineEdit(self)
@@ -40,19 +71,21 @@ class Window(QtWidgets.QMainWindow):
         self.idLayout.addWidget(self.idLayoutText)
         self.idLayout.addWidget(self.idLayoutTextSetBox)
 
-        self.gradeLayouts = []
-        self.gradeLayoutSetBoxes = []
-        for i in range(constants.NUM_GRADES):
-            gradeLayoutText = QLabel("Grade {}: ".format(i + 1), self)
-            gradeLayoutTextSetBox = QLineEdit(self)
-            gradeLayoutTextSetBox.setText("0")
-            self.gradeLayoutSetBoxes.append(gradeLayoutTextSetBox)
+        self.feedbackLayoutText = QLabel("Feedback: ", self)
+        self.feedbackLayoutTextSetBox = QLineEdit(self)
+        self.feedbackLayoutTextSetBox.setText("")
 
-            gradeLayout = QHBoxLayout(self)
-            gradeLayout.addWidget(gradeLayoutText)
-            gradeLayout.addWidget(gradeLayoutTextSetBox)
+        self.feedbackLayout = QHBoxLayout(self)
+        self.feedbackLayout.addWidget(self.feedbackLayoutText)
+        self.feedbackLayout.addWidget(self.feedbackLayoutTextSetBox)
 
-            self.gradeLayouts.append(gradeLayout)
+        self.gradeLayoutText = QLabel("Grade: ", self)
+        self.gradeLayoutTextSetBox = QLineEdit(self)
+        self.gradeLayoutTextSetBox.setText("0")
+
+        self.gradeLayout = QHBoxLayout(self)
+        self.gradeLayout.addWidget(self.gradeLayoutText)
+        self.gradeLayout.addWidget(self.gradeLayoutTextSetBox)
 
         self.submitButton = QPushButton('Submit Grades', self)
         self.nextButton = QPushButton('Next Student', self)
@@ -61,14 +94,12 @@ class Window(QtWidgets.QMainWindow):
         self.verticalLayout = QVBoxLayout(self)
         self.verticalLayout.addLayout(self.usernameLayout)
         self.verticalLayout.addLayout(self.idLayout)
+        self.verticalLayout.addLayout(self.feedbackLayout)
+        self.verticalLayout.addLayout(self.gradeLayout)
 
-        for layout in self.gradeLayouts:
-            self.verticalLayout.addLayout(layout)
         self.verticalLayout.addWidget(self.submitButton)
         self.verticalLayout.addWidget(self.nextButton)
         self.verticalLayout.addWidget(self.compileButton)
-
-
 
         self.submitButton.clicked.connect(self.submitGrades)
         self.nextButton.clicked.connect(self.loadNextStudent)
@@ -78,91 +109,110 @@ class Window(QtWidgets.QMainWindow):
         self.setCentralWidget(widget)
         widget.setLayout(self.verticalLayout)
 
-
-
         self.loadNextStudent()
 
-    #this function should write out grades to each folder in a csv file per student
+    # this function should write out grades to each folder in a csv file per student
     def submitGrades(self):
-        #used to specify Order of output
-        columnNames = ["Student ID"]
+        # used to specify Order of output
 
-        #build up a pandas dataframe (NEEDS WORK)
-        data = {'Student ID':  [self.idLayoutTextSetBox.text()]}
-        for i, layout in enumerate(self.gradeLayoutSetBoxes):
-            columnName = "Grade {}".format(i+1)
-            data[columnName] = [self.gradeLayoutSetBoxes[i].text()]
-            columnNames.append(columnName)
-
+        columnNames = ["Student ID", "Grade", "Feedback"]
+        data = {'Student ID': [self.idLayoutTextSetBox.text()], "Grade": [self.gradeLayoutTextSetBox.text()],
+                "Feedback": [self.feedbackLayoutTextSetBox.text()]}
 
         df = pd.DataFrame(data)
-        gradeHelperCSVPath = os.path.join(constants.LAB_DIRECTORY, self.studentDirectories[self.currentStudentGradedIndex], "gradeHelper.csv")
+        gradeHelperCSVPath = os.path.join(constants.LAB_DIRECTORY,
+                                          self.studentDirectories[self.currentStudentGradedIndex], "gradeHelper.csv")
 
-
-        df.to_csv(gradeHelperCSVPath, columns = columnNames)
+        df.to_csv(gradeHelperCSVPath, columns=columnNames)
 
         self.currentStudentGradesSubmitted = True
 
-
-    #this function should get information for next student in the list that doesn't have a CSV already stored in the folder
-    #if there is a grade csv already in the folder then load someone else
-
+    # Gets information for next student in the list that doesn't have a CSV already stored in the folder and replaces
+    # working directory with next student's files
     def loadNextStudent(self):
-        print("Write out csv for blah")
-        print("Grade 0 can be asccessed with", self.gradeLayoutSetBoxes[0].text())
+        def binarySearch(students, studentName):
+            i = bisect_left(students, studentName)
+            if i != len(students) and students[i] == studentName:
+                return i
+            else:
+                return -1
 
+        if self.currentStudentGradesSubmitted or self.programStarted:
+            nextUngradedStudent = next(filter(lambda studentDirectory:
+                                              not os.path.exists(os.path.join(constants.LAB_DIRECTORY, studentDirectory,
+                                                                              "gradeHelper.csv")),
+                                              self.studentDirectories), None)
+            if not nextUngradedStudent:
+                self.gradedStudentDirectories[0:len(self.studentDirectories)] = [True] * len(self.studentDirectories)
+                createMessagePopUpBox("Done grading all students. You can compile the report now")
+                return
 
-
-        #this function shouldn't go load new student info
-        #unless submit was clicked or we are just loading into the program(first person laoding)
-        if(self.currentStudentGradesSubmitted or self.programStarted):
-            #find the next ungraded student index
-            nextUngradedStudent = self.currentStudentGradedIndex
-
-            #find next ungraded student ID
-            while nextUngradedStudent <= len(self.studentDirectories):
-                #build up the path for where gradeHelper.csv should exist
-                gradeHelperCSVPath = os.path.join(constants.LAB_DIRECTORY, self.studentDirectories[nextUngradedStudent], "gradeHelper.csv")
-                if os.path.exists(gradeHelperCSVPath):
-                    nextUngradedStudent+=1
-                else:
-                    break
-
-            self.currentStudentGradedIndex = nextUngradedStudent
-            self.loadStudentInfo()
+            self.currentStudentGradedIndex = binarySearch(self.studentDirectories, nextUngradedStudent)
+            # Done so list of bools is updated on startup
+            self.gradedStudentDirectories[0:self.currentStudentGradedIndex] = [True] * self.currentStudentGradedIndex
+            self.loadStudentInfo(nextUngradedStudent)
+            self.loadDirectoryWithStudentFiles(nextUngradedStudent)
             self.clearGrades()
         else:
-            print("NOT SUBMITTED YET") #THIS SHOULD GO TO PYQT CONSOLE SOMEWHERE - WORK ON THIS
-
+            createMessagePopUpBox("Grades have not been submitted yet")
         self.programStarted = False
 
-    #load all info into form for current student
-    #WORK ON THIS
-    def loadStudentInfo(self):
-        currentUsername = self.studentDirectories[self.currentStudentGradedIndex]
-        self.usernameLayoutTextSetBox.setText(currentUsername)
+    # load all info into form for current student
+    def loadStudentInfo(self, studentUsername):
+        if not studentUsername:
+            createMessagePopUpBox("Could not find next student")
 
-        #copy the python files in directory to working directory(sometimes students have helper files so copy all files)
-        #overwrite similiar named files in working directory
+        filePath = os.path.join(constants.LAB_DIRECTORY, studentUsername,
+                                constants.FILE_NAME)
+        file = open(filePath, "rb")
+        commentWithStudentID = None
+        for tokenType, token, start, end, line in tokenize.tokenize(file.readline):
+            if tokenType == tokenize.COMMENT and "id" in token.lower() and "student" in token.lower():
+                commentWithStudentID = token
+        if not commentWithStudentID:
+            createMessagePopUpBox(f"Check {studentUsername}'s folder. Could not find student ID")
+            return
+        studentID = findStudentID(commentWithStudentID)
 
-        #look for file that matches "lab4.py"
-        #this is in constants.FILE_NAME
-        #read info from it
+        self.usernameLayoutTextSetBox.setText(studentUsername)
+        self.idLayoutTextSetBox.setText(studentID)
 
-    #function to clear grades in the textboxes
+    def loadDirectoryWithStudentFiles(self, studentUsername):
+        deleteDirectoryContent(os.path.join(constants.WORKING_DIRECTORY))
+        try:
+            shutil.copytree(os.path.join(constants.LAB_DIRECTORY, studentUsername),
+                            os.path.join(constants.WORKING_DIRECTORY),
+                            dirs_exist_ok=True)
+        except Exception as e:
+            createMessagePopUpBox(
+                f"Check the student directory; Could not load student's directory into "
+                f"working directory: {constants.WORKING_DIRECTORY}")
+
+    # function to clear grades in the textboxes
     def clearGrades(self):
-        print("CLEAR GRADES")
+        self.gradeLayoutTextSetBox.setText("0")
 
-
-    #function will run through all directories in self.studentDirectories load the csvs and output a final csv in the eclass format
+    # function will run through all directories in self.studentDirectories load the csvs and output a final csv in the eclass format
     def compileReport(self):
-        print("FUNCTION WILL DO STUFF SOON")
+        classGrades = []
+        compiledReportPath = "FinalGrades.csv"
+        for i, x in enumerate(self.studentDirectories):
+            perStudentCSVPath = os.path.join(constants.LAB_DIRECTORY,
+                                             self.studentDirectories[i], "gradeHelper.csv")
+            if self.gradedStudentDirectories[i]:
+                try:
+                    with open(perStudentCSVPath) as f:
+                        reader = list(csv.reader(f, delimiter=','))[1:][0][1:]
+                        studentInfo = {'studentId': reader[0], "grade": reader[1], "feedback": reader[2]}
+                        classGrades.append(studentInfo)
+                except Exception as e:
+                    createMessagePopUpBox(str(e))
 
-
+        df = pd.DataFrame(classGrades).set_index('studentId')
+        df.to_csv(compiledReportPath)
 
 
 app = QtWidgets.QApplication([])
 w = Window()
 w.show()
 app.exec_()
-
