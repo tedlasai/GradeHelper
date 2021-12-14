1be7425f31216a53ad5e2430e2416c7257505507
JustinSo1 <sojustin28@gmail.com>
2021-12-14 13:06:21 -0500

    Add comments to constants

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
-LAB_DIRECTORY = "lab4"
-WORKING_DIRECTORY = "WD"
-FILE_NAME = "lab4.py"
+LAB_DIRECTORY = "lab4"  # Directory with all the student files
+WORKING_DIRECTORY = "WD"  # Directory with all the current student's files
+FILE_NAME = "lab4.py"  # File name with the student credentials
 CSV_FILE_NAME = "gradeHelper.csv"
 FINAL_GRADE_REPORT_PATH = "FinalGrades.csv"
-SCALE_FACTOR = 10
-TASKS = 2
+SCALE_FACTOR = 1  # Multiply the grade by this
+TASKS = 3  # Num of tasks to grade
--- merge_csv.py
+++ merge_csv.py
@@ -4,7 +4,7 @@ import pandas as pd
 def merge_csv(csv1, csv2, output_file):
     data1 = pd.read_csv(csv1)
     data2 = pd.read_csv(csv2)
-    output = pd.merge(data1, data2, on=["First name", "Surname", "ID number", "Feedback"])
+    output = pd.merge(data1, data2, on=["First name", "Surname", "ID number"])
     output.to_csv(output_file, index=False)
 
 
