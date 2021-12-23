e3545943704befde7a804cdbd125b32f35ba891b
JustinSo1 <sojustin28@gmail.com>
2021-12-22 20:38:57 -0500

    Allow the 2 Feedback columns to be merged into a singular feedback column

--- merge_csv.py
+++ merge_csv.py
@@ -1,11 +1,20 @@
 import pandas as pd
 
 
-def merge_csv(csv1, csv2, output_file):
-    data1 = pd.read_csv(csv1)
-    data2 = pd.read_csv(csv2)
-    output = pd.merge(data1, data2, on=["First name", "Surname", "ID number"])
-    output.to_csv(output_file, index=False)
+def merge_csv(csv1, csv2):
+    return pd.merge(csv1, csv2, on=["First name", "Surname", "ID number"])
 
 
-merge_csv("FinalGrades.csv", "FinalGrades1.csv", "Final_grades_merged.csv")
+def merge_feedback_columns(df, columns):
+    feedback_x, feedback_y = columns
+    df["Feedback"] = df[feedback_x].map(str) + " And " + df[feedback_y].map(str)
+    df = df.drop(feedback_x, axis=1)
+    df = df.drop(feedback_y, axis=1)
+    return df
+
+
+data1 = pd.read_csv("FinalGrades.csv")
+data2 = pd.read_csv("FinalGrades1.csv")
+output = merge_csv(data1, data2)
+output = merge_feedback_columns(output, ["Feedback_x", "Feedback_y"])
+output.to_csv("Final_grades_merged.csv", index=False)
